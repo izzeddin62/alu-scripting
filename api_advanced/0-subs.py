a3afa6c116e324a868c8e4481d065c1087606478
izzeddin62 <i.ishimwe@alustudent.com>
2022-10-19 00:00:36 +0200

    added docs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""get subscribers function""" 
 
 import json
 import requests
 import sys
 def number_of_subscribers(subreddit):
+    """get all subscribers"""
     if len(sys.argv) < 2:
         return 0
     else:
