20495cf89d4ba875e69b2809ad0cd9b21fa0f443
izzeddin62 <i.ishimwe@alustudent.com>
2022-10-20 23:57:18 +0200

    return all post

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -7,17 +7,21 @@ import requests
 import sys
 
 
-def recurse(subreddit,  hot_list=[]):
-    """get top ten hot post"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=200".format(subreddit)
+def recurse(subreddit,  hot_list=[], after=None):
+    """get top all hot post"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    result = requests.get(url, headers=headers, allow_redirects=False)
+    result = requests.get(url,
+                          headers=headers,
+                          params={"after": after},
+                          allow_redirects=False)
     listing = []
     if result.status_code != 200:
         return None
     body = json.loads(result.text)
-    if "after" in body["data"]:
-        return recurse(subreddit, hot_list + body["data"]["children"])
+    if body["data"]["after"] is not None:
+        children = body["data"]["children"]
+        newlist = hot_list + [i["data"]["title"] for i in children]
+        return recurse(subreddit, newlist, body["data"]["after"])
     else:
         return hot_list
-        
