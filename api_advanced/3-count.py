6ac043b429a2613c385fb62e132e3a0dadce0baf
izzeddin62 <i.ishimwe@alustudent.com>
2022-10-22 14:09:46 +0200

    sort the return list

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -34,9 +34,18 @@ def count_words(subreddit, word_list, after=None):
                         i["count"] = i["count"] + 1
         return count_words(subreddit, newlist, body["data"]["after"])
     else:
+        newlist = word_list
+        if type(word_list[0]) is str:
+            unique = list(dict.fromkeys([i.lower() for i in word_list]))
+            newlist = [{"key": i, "count": 0} for i in unique]
+        for i in newlist:
+            for j in body["data"]["children"]:
+                for k in j["data"]["title"].lower().split():
+                    if i["key"] == k:
+                        i["count"] = i["count"] + 1
         key = operator.itemgetter("count", "key")
-        sorted_list = sorted(word_list, key=key)
-        sorted_list.reverse()
+        sorted_list = sorted(word_list, key=key, reverse=True)
+        word_list = sorted_list
         for i in sorted_list:
             if i["count"] > 0:
                 print("{}: {}".format(i["key"], i["count"]))
